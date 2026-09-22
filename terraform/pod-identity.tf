f92d0012b5b5ee0a2f33e575ecc8d616e2e1c5ab
Vamshi <73492940+vamshiearth@users.noreply.github.com>
2026-09-22 16:32:00 -0400

    fix EKS bootstrap and EBS CSI pod identity

--- terraform/pod-identity.tf
+++ terraform/pod-identity.tf
@@ -212,3 +212,43 @@ resource "aws_eks_pod_identity_association" "grafana" {
 
   depends_on = [aws_eks_addon.pod_identity_agent]
 }
+
+data "aws_iam_policy_document" "ebs_csi_pod_identity_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["pods.eks.amazonaws.com"]
+    }
+
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession"
+    ]
+  }
+}
+
+resource "aws_iam_role" "ebs_csi_pod_identity" {
+  name               = "${local.name_prefix}-ebs-csi-pod-identity"
+  assume_role_policy = data.aws_iam_policy_document.ebs_csi_pod_identity_assume_role.json
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_cluster_scoped" {
+  role       = aws_iam_role.ebs_csi_pod_identity.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEBSCSIDriverEKSClusterScopedPolicy"
+}
+
+resource "aws_eks_pod_identity_association" "ebs_csi" {
+  cluster_name    = aws_eks_cluster.cloudpilot.name
+  namespace       = "kube-system"
+  service_account = "ebs-csi-controller-sa"
+  role_arn        = aws_iam_role.ebs_csi_pod_identity.arn
+
+  depends_on = [
+    aws_eks_addon.pod_identity_agent,
+    aws_iam_role_policy_attachment.ebs_csi_cluster_scoped
+  ]
+}
